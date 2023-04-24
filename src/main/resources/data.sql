9394bf1bf55d81bf49f6fc8bde46ad4c33161f4d
vladislavStel <stelmach.vladislav@yandex.ru>
2023-04-24 19:19:50 +0300

    refactor: config db

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO MPA (mpa_id, name) VALUES ('1', 'G');
+INSERT INTO MPA (mpa_id, name) VALUES ('2', 'PG');
+INSERT INTO MPA (mpa_id, name) VALUES ('3', 'PG-13');
+INSERT INTO MPA (mpa_id, name) VALUES ('4', 'R');
+INSERT INTO MPA (mpa_id, name) VALUES ('5', 'NC-17');
+
+INSERT INTO GENRES (genre_id, name) VALUES ('1', 'Комедия');
+INSERT INTO GENRES (genre_id, name) VALUES ('2', 'Драма');
+INSERT INTO GENRES (genre_id, name) VALUES ('3', 'Мультфильм');
+INSERT INTO GENRES (genre_id, name) VALUES ('4', 'Триллер');
+INSERT INTO GENRES (genre_id, name) VALUES ('5', 'Документальный');
+INSERT INTO GENRES (genre_id, name) VALUES ('6', 'Боевик');
