00d43a1b2b2986754d02adadddc358536b186ae7
vladislavStel <stelmach.vladislav@yandex.ru>
2023-05-02 18:51:55 +0300

    refactor: add new model

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Genre {
+
+    int id;
+    String name;
+
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Mpa.java
@@ -0,0 +1,20 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Mpa {
+
+    int id;
+    String name;
+
+    public Mpa(int id) {
+        this.id = id;
+    }
+
+}
