0eed1bbe21a624cfe7b43427612a2b6c99c579a0
cam1234251 <cam1234251@mail.ru>
2023-05-25 14:04:41 +0300

    Fix Unused import

--- src/main/java/ru/yandex/practicum/filmorate/service/impl/UserServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/impl/UserServiceImpl.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.ObjectNotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
-import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
