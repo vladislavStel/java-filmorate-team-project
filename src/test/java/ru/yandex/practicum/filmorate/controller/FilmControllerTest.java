cc6d740158b55dda601536dd9814209510a3a855
cam1234251 <cam1234251@mail.ru>
2023-05-25 14:09:50 +0300

    Corrected FilmTest

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -87,7 +87,7 @@ class FilmControllerTest {
         Film film2 = filmBuilder.id(2L).name("Film name2").build();
         when(filmService.getPopular(2L, 2L, 2000)).thenReturn(List.of(film1, film2));
 
-        mockMvc.perform(get(url + "/popular?count=2"))
+        mockMvc.perform(get(url + "/popular?count=2&year=2000&genreId=2"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].id", is(1)))
