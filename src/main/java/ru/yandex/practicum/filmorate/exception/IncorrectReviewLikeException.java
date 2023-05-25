e8ab21e9ecc81cc4f00286eb6a26e64cdc209b27
Nevsky <nevisky@mail.ru>
2023-05-25 23:33:11 +0300

    fix:fix spaces

--- src/main/java/ru/yandex/practicum/filmorate/exception/IncorrectReviewLikeException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/IncorrectReviewLikeException.java
@@ -1,8 +1,6 @@
 package ru.yandex.practicum.filmorate.exception;
 
-
-
-public class IncorrectReviewLikeException extends RuntimeException{
+public class IncorrectReviewLikeException extends RuntimeException {
 
     public IncorrectReviewLikeException(String message) {
         super(message);
