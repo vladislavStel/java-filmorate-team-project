361277bb0d4d8fb9ee46916043b5d2c7cdf09e8c
Nevsky <nevisky@mail.ru>
2023-05-28 00:57:15 +0300

    fix: fix space

--- src/main/java/ru/yandex/practicum/filmorate/service/impl/FilmServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/impl/FilmServiceImpl.java
@@ -98,7 +98,6 @@ public class FilmServiceImpl implements FilmService {
         if (directorStorage.isNotExistsDirector(directorId)) {
             throw new ObjectNotFoundException(String.format("Не найден режиссер: id=%d", directorId));
         }
-
         return filmStorage
                 .findFilmsByDirectorSorted(directorId, sortBy)
                 .stream()
